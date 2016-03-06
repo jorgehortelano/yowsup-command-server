7f7a3370f8701e29582a3cea9892f7ccd1c4396d
Jorge Hortelano <jhortelano@biit-solutions.com>
2016-03-06 11:31:23 +0100

    Updated project structure to run propertly.

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 from __future__ import print_function
 from setuptools import setup, find_packages
 
-deps = ['yowsup2==2.3.185', 'yowlayer-store==0.1', 'cleverbot']
+deps = ['yowsup2==2.4.48', 'yowlayer-store==0.1', 'commandserver']
 
 setup(
-    name='yowlayer-commandserver',
+    name='yowsup-commandserver',
     version="0.1",
     tests_require=[],
     install_requires = deps,
     dependency_links = [
-        'https://github.com/tgalal/yowsup/archive/develop.zip#egg=yowsup2-2.3.185',
+        'https://github.com/tgalal/yowsup/archive/develop.zip#egg=yowsup2-2.4.48',
         'https://github.com/tgalal/yowlayer-store/archive/master.zip#egg=yowlayer-store-0.1'
     ],
     packages= find_packages(),
     include_package_data=True,
     platforms='any',
-    namespace_packages = ['yowsup_ext.commandserver', 'yowsup_ext.layers'],
+    namespace_packages = ['yowsup_ext', 'yowsup_ext.commandserver', 'yowsup_ext.layers'],
     classifiers = [
         'Programming Language :: Python',
         'Development Status :: 4 - Beta',

a46e6cb081c1ebef2f9dd3c1a7d3dec7fdd608eb
Jorge Hortelano <jhortelano@biit-solutions.com>
2016-03-25 16:13:15 +0100

    Updated install script

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from setuptools import setup, find_packages
+import yowsup_ext.commandserver
 
 deps = ['yowsup2==2.4.48', 'yowsup-commandserver']
 
 setup(
     name='yowsup-commandserver',
-    version="0.1",
+    version=yowsup_ext.commandserver.__version__,
     tests_require=[],
     install_requires = deps,
+    scripts = ['yowsup-commandserver'],
     dependency_links = [
         'https://github.com/tgalal/yowsup/archive/develop.zip#egg=yowsup2-2.4.48',
     ],
